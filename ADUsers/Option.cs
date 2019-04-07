51bb61d70a2ff132bc2a2d4a1ea859c8e6a1afcc
Frédéric Le Coquil <flecoqui@microsoft.com>
2019-04-07 21:51:18 +0200

    First commit

---
+++ ADUsers/Option.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ADUsers
+{
+    class Option
+    {
+        public enum Action
+        {
+            None = 0,
+            Help,
+            List,
+            Add,
+            Remove
+        }
+        public enum LogLevel
+        {
+            None = 0,
+            Error,
+            Information,
+            Warning,
+            Verbose
+        }
+        public Action ADUserAction { get; set; }
+        public string ldapDomain { get; set; }
+        public string ldapPath { get; set; }
+
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string description { get; set; }
+        public LogLevel ConsoleLevel { get; set; }
+        public LogLevel TraceLevel { get; set; }
+        public string TraceFile { get; set; }
+        public int TraceSize { get; set; }
+        private string ErrorMessage = string.Empty;
+        public string GetErrorMessage()
+        {
+            return ErrorMessage;
+        }
+        public Option()
+        {
+            this.firstName = string.Empty;
+            this.lastName = string.Empty;
+            this.userName = string.Empty;
+            this.password = string.Empty;
+            this.ldapDomain = string.Empty;
+            this.ldapPath = string.Empty;
+            this.TraceFile = string.Empty;
+            this.TraceSize = 524280;
+            this.TraceLevel = LogLevel.Information;
+            this.ConsoleLevel = LogLevel.Information;
+        }
+        private string Version = "1.0.0.1";
+        private string InformationMessage = "ADUser:\r\n" + "Version: {0} \r\n" + "Syntax:\r\n" +
+    "ADUser --list --domain <domain-for instance: testwvd.pw> --ldappath <LDAP Path-for instance: CN=Users,DC=testwvd,DC=pw>\r\n" +
+    "ADUser --add  --domain <domain-for instance: testwvd.pw> --ldappath <LDAP Path-for instance: CN=Users,DC=testwvd,DC=pw>\r\n" +
+    "                 [--firstname <firstname> ]\r\n" +
+    "                 [--lastname <lastname> ]\r\n" +
+    "                 [--description <description> ]\r\n" +
+    "                 [--username <username> ]\r\n" +
+    "                 [--password <password> ]\r\n" +
+    "                 [--tracefile <path> --tracesize <size in bytes> --tracelevel <none|error|information|warning|verbose>]\r\n" +
+    "                 [--consolelevel <none|error|information|warning|verbose>]\r\n" +
+    "ADUser --remove --domain <domain>  --ldappath <LDAP Path>  \r\n" +
+    "                 [--username <username> ]\r\n" +
+    "                 [--tracefile <path> --tracesize <size in bytes> --tracelevel <none|error|information|warning|verbose>]\r\n" +
+    "                 [--consolelevel <none|error|information|warning|verbose>]\r\n" +
+    "ADUser --help\r\n";
+        public void PrintHelp()
+        {
+            LogInformation(string.Format(InformationMessage, Version));
+        }
+        void LogMessage(LogLevel level, string Message)
+        {
+            string Text = string.Empty;
+            if ((level <= TraceLevel) && (!string.IsNullOrEmpty(this.TraceFile)))
+            {
+                Text = string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " " + Message + "\r\n";
+                LogTrace(this.TraceFile, this.TraceSize, Text);
+            }
+            if (level <= ConsoleLevel)
+            {
+                if (string.IsNullOrEmpty(Text))
+                    Text = string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " " + Message + "\r\n";
+                Console.Write(Text);
+            }
+        }
+        public void LogVerbose(string Message)
+        {
+            LogMessage(LogLevel.Verbose, Message);
+        }
+        public void LogInformation(string Message)
+        {
+            LogMessage(LogLevel.Information, Message);
+        }
+        public void LogWarning(string Message)
+        {
+            LogMessage(LogLevel.Warning, Message);
+        }
+        public void LogError(string Message)
+        {
+            LogMessage(LogLevel.Error, Message);
+        }
+        static public char GetChar(byte b)
+        {
+            if ((b >= 32) && (b < 127))
+                return (char)b;
+            return '.';
+        }
+        static public string DumpHex(byte[] data)
+        {
+            string result = string.Empty;
+            string resultHex = " ";
+            string resultASCII = " ";
+            int Len = ((data.Length % 16 == 0) ? (data.Length / 16) : (data.Length / 16) + 1) * 16;
+            for (int i = 0; i < Len; i++)
+            {
+                if (i < data.Length)
+                {
+                    resultASCII += string.Format("{0}", GetChar(data[i]));
+                    resultHex += string.Format("{0:X2} ", data[i]);
+                }
+                else
+                {
+                    resultASCII += " ";
+                    resultHex += "   ";
+                }
+                if (i % 16 == 15)
+                {
+                    result += string.Format("{0:X8} ", i - 15) + resultHex + resultASCII + "\r\n";
+                    resultHex = " ";
+                    resultASCII = " ";
+                }
+            }
+            return result;
+        }
+        public ulong LogTrace(string fullPath, long Tracefile, string Message)
+        {
+            ulong retVal = 0;
+
+            try
+            {
+                lock (this)
+                {
+                    FileStream fs = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    if (fs != null)
+                    {
+                        long pos = fs.Seek(0, SeekOrigin.End);
+                        byte[] data = UTF8Encoding.UTF8.GetBytes(Message);
+                        if (data != null)
+                        {
+                            if (pos + data.Length > Tracefile)
+                                fs.SetLength(0);
+                            fs.Write(data, 0, data.Length);
+                            retVal = (ulong)data.Length;
+                        }
+                        fs.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception while append in file:" + fullPath + " Exception: " + ex.Message);
+            }
+            return retVal;
+        }
+        static public LogLevel GetLogLevel(string text)
+        {
+            LogLevel level = LogLevel.None;
+            switch (text.ToLower())
+            {
+                case "none":
+                    level = LogLevel.None;
+                    break;
+                case "information":
+                    level = LogLevel.Information;
+                    break;
+                case "error":
+                    level = LogLevel.Error;
+                    break;
+                case "warning":
+                    level = LogLevel.Warning;
+                    break;
+                case "verbose":
+                    level = LogLevel.Verbose;
+                    break;
+                default:
+                    break;
+            }
+            return level;
+        }
+        public static Option ParseCommandLine(string[] args)
+        {
+            Option option = new Option();
+
+            if (option == null) 
+            {
+                return null;
+            }
+            try
+            {
+
+                option.TraceFile = "ADUser.log";
+                if (args != null)
+                {
+
+                    int i = 0;
+                    if (args.Length == 0)
+                    {
+                        option.ErrorMessage = "No parameter in the command line";
+                        return option;
+                    }
+                    while ((i < args.Length) && (string.IsNullOrEmpty(option.ErrorMessage)))
+                    {
+                        switch (args[i++])
+                        {
+
+                            case "--help":
+                                option.ADUserAction = Action.Help;
+                                break;
+                            case "--add":
+                                option.ADUserAction = Action.Add;
+                                break;
+                            case "--list":
+                                option.ADUserAction = Action.List;
+                                break;
+                            case "--remove":
+                                option.ADUserAction = Action.Remove;
+                                break;
+                            case "--firstname":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.firstName = args[i++];
+                                else
+                                    option.ErrorMessage = "first name not set";
+                                break;
+                            case "--lastname":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.lastName = args[i++];
+                                else
+                                    option.ErrorMessage = "last name not set";
+                                break;
+                            case "--username":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.userName = args[i++];
+                                else
+                                    option.ErrorMessage = "user name not set";
+                                break;
+                            case "--password":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.password = args[i++];
+                                else
+                                    option.ErrorMessage = "password not set";
+                                break;
+                            case "--description":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.description = args[i++];
+                                else
+                                    option.ErrorMessage = "description not set";
+                                break;
+                            case "--domain":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.ldapDomain = args[i++];
+                                else
+                                    option.ErrorMessage = "domain not set";
+                                break;
+                            case "--ldappath":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.ldapPath = args[i++];
+                                else
+                                    option.ErrorMessage = "ldap path not set";
+                                break;
+                            case "--tracefile":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.TraceFile = args[i++];
+                                else
+                                    option.ErrorMessage = "TraceFile not set";
+                                break;
+                            case "--tracesize":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                {
+                                    int tracesize = 0;
+                                    if (int.TryParse(args[i++], out tracesize))
+                                        option.TraceSize = tracesize;
+                                    else
+                                        option.ErrorMessage = "TraceSize value incorrect";
+                                }
+                                else
+                                    option.ErrorMessage = "TraceSize not set";
+                                break;
+                            case "--tracelevel":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.TraceLevel = GetLogLevel(args[i++]);
+                                else
+                                    option.ErrorMessage = "TraceLevel not set";
+                                break;
+                            case "--consolelevel":
+                                if ((i < args.Length) && (!string.IsNullOrEmpty(args[i])))
+                                    option.ConsoleLevel = GetLogLevel(args[i++]);
+                                else
+                                    option.ErrorMessage = "ConsoleLevel not set";
+                                break;
+                            default:
+                                option.ErrorMessage = "wrong parameter: " + args[i - 1];
+                                return option;
+                        }
+                    }
+
+                    if (option.ADUserAction == Action.None)
+                    {
+                        option.ErrorMessage = "No feature in the command line";
+                        return option;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                option.ErrorMessage = "Exception while analyzing the options: " + ex.Message;
+                return option;
+            }
+
+            if (!string.IsNullOrEmpty(option.ErrorMessage))
+            {
+                return option;
+            }
+            return CheckOptions(option);
+        }
+        public static Option CheckOptions(Option option)
+        {
+            if (option.ADUserAction == Action.Help) 
+            {
+                return option;
+            }
+            else if (option.ADUserAction == Action.Add)
+            {
+                if (
+                    (!string.IsNullOrEmpty(option.firstName)) &&
+                    (!string.IsNullOrEmpty(option.lastName)) &&
+                    (!string.IsNullOrEmpty(option.ldapDomain)) &&
+                    (!string.IsNullOrEmpty(option.ldapPath)) &&
+                    (!string.IsNullOrEmpty(option.description)) &&
+                    (!string.IsNullOrEmpty(option.userName)) &&
+                    (!string.IsNullOrEmpty(option.password)) 
+                    )
+                {
+
+                    return option;
+                }
+                else
+                    option.ErrorMessage = "Missing parameters for Add feature";
+            }
+            else if (option.ADUserAction == Action.Remove)
+            {
+                if (
+                    (!string.IsNullOrEmpty(option.ldapDomain)) &&
+                    (!string.IsNullOrEmpty(option.ldapPath)) &&
+                    (!string.IsNullOrEmpty(option.userName)) 
+                    )
+                {
+
+                    return option;
+                }
+                else
+                    option.ErrorMessage = "Missing parameters for Remove feature";
+            }
+            else if (option.ADUserAction == Action.List)
+            {
+                if (
+                    (!string.IsNullOrEmpty(option.ldapDomain)) &&
+                    (!string.IsNullOrEmpty(option.ldapPath)) 
+                    )
+                {
+
+                    return option;
+                }
+                else
+                    option.ErrorMessage = "Missing parameters for List feature";
+            }
+            return null;
+        }
+    }
+}
---
+++ ADUsers/Program.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace ADUsers
+{
+    class Program
+    {
+        private static string sDomain = @"testwvd.pw";
+        private static string sDefaultOU = @"OU=Users,OU=testwvd,DC=testwvd,DC=pw";
+        private static string sServiceUser = @"TESTWVD\VMAdmin";
+        private static string sServicePassword = @"VMP@ssw0rd123";
+        public static PrincipalContext GetPrincipalContext()
+        {
+            PrincipalContext oPrincipalContext = new PrincipalContext
+               (ContextType.Domain, sDomain, sDefaultOU,
+               //ContextOptions.SimpleBind,
+                                sServiceUser, sServicePassword);
+            Console.WriteLine("GetPrincipalContext success");
+            return oPrincipalContext;
+        }
+        public static UserPrincipal GetUser(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = null;
+            try
+            {
+                PrincipalContext oPrincipalContext = GetPrincipalContext();
+                Console.WriteLine(" fining");
+                oUserPrincipal =
+                   UserPrincipal.FindByIdentity(oPrincipalContext, sUserName);
+                Console.WriteLine(" fining done");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception while looking for user " + sUserName + ": " + ex.Message);
+
+            }
+            return oUserPrincipal;
+        }
+
+        public static bool IsUserExisiting(string sUserName)
+        {
+            if (GetUser(sUserName) == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static UserPrincipal CreateNewUser(string sUserName, string sPassword, string sGivenName, string sSurname, string sEmailAddress)
+        {
+            if (!IsUserExisiting(sUserName))
+            {
+                PrincipalContext oPrincipalContext = GetPrincipalContext();
+                Console.WriteLine(" creating context");
+                UserPrincipal oUserPrincipal = new UserPrincipal(oPrincipalContext);
+                if (oUserPrincipal != null)
+                {
+                    Console.WriteLine(" creating user");
+
+                    oUserPrincipal.UserPrincipalName = sUserName;
+                    oUserPrincipal.SetPassword(sPassword);
+                    oUserPrincipal.GivenName = sGivenName;
+                    oUserPrincipal.Surname = sSurname;
+                    oUserPrincipal.EmailAddress = sEmailAddress;
+                    oUserPrincipal.DisplayName = sGivenName + " " + sSurname;
+                    oUserPrincipal.Name = sGivenName + " " + sSurname;
+                    oUserPrincipal.ExpirePasswordNow();
+                    //                    oUserPrincipal..PasswordNeverExpires = true;
+                    Console.WriteLine(" saving user");
+                    oUserPrincipal.Save();
+                }
+                return oUserPrincipal;
+            }
+            else
+            {
+                return GetUser(sUserName);
+            }
+        }
+        public static bool DeleteUser(string sUserName)
+        {
+            try
+            {
+                UserPrincipal oUserPrincipal = GetUser(sUserName);
+                if (oUserPrincipal != null)
+                {
+                    oUserPrincipal.Delete();
+                    return true;
+                }
+            }
+            catch
+            {
+            }
+            return false;
+        }
+        static DirectoryEntry createDirectoryEntry(string domain, string ldapPath)
+        {
+            // create and return new LDAP connection with desired settings  
+
+            DirectoryEntry ldapConnection = new DirectoryEntry(domain);
+            ldapConnection.Path = "LDAP://" + ldapPath;
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+            return ldapConnection;
+        }
+        static int deleteUser(DirectoryEntry myLdapConnection, String first,
+                      String last )
+        {
+            DirectoryEntry user = myLdapConnection.Children.Find("CN=" + first + " " + last, "user");
+            if (user != null)
+            {
+                myLdapConnection.Children.Remove(user);
+                user.CommitChanges();
+            }
+            return 0;
+        }
+        static int createUser(DirectoryEntry myLdapConnection, String domain, String first,
+                      String last, String description, object[] password,
+                      String username,  bool enabled)
+        {
+            // create new user object and write into AD  
+
+            DirectoryEntry user = myLdapConnection.Children.Add(
+                                  "CN=" + username, "user");
+
+            // User name (domain based)   
+            user.Properties["userprincipalname"].Add(username + "@" + domain);
+
+            // User name (older systems)  
+            user.Properties["samaccountname"].Add(username);
+
+            // Surname  
+            user.Properties["sn"].Add(last);
+
+            // Forename  
+            user.Properties["givenname"].Add(first);
+
+            // Display name  
+            user.Properties["displayname"].Add(first + " " + last);
+
+            // Description  
+            user.Properties["description"].Add(description);
+
+            // E-mail  
+            user.Properties["mail"].Add(username + "@" + domain);
+
+
+            user.CommitChanges();
+
+            
+            // enable account if requested (see http://support.microsoft.com/kb/305144 for other codes)   
+            //if (enabled)
+            //    user.Invoke("Put", new object[] { "userAccountControl", "512" });
+            user.Properties["userAccountControl"].Value = 0x0200 | 0x10000;
+
+            // set user's password  
+            user.Invoke("SetPassword", password);
+
+            user.CommitChanges();
+
+
+            return 0;
+        }
+        static void ListUsers(Option option, string ldapDomain, string ldapPath)
+        {
+            try
+            {
+
+                option.LogInformation("Creating the LDAP Connection for domain " + ldapDomain + " and LDAP://" + ldapPath);
+                DirectoryEntry myLdapConnection = createDirectoryEntry(ldapDomain, ldapPath);
+
+                if (myLdapConnection != null)
+                {
+                    option.LogInformation("Listing users");
+                    foreach (DirectoryEntry entry in myLdapConnection.Children)
+                    {
+                        if (entry.SchemaClassName == "user")
+                            option.LogInformation("User Name: " + entry.Username + " Path: " + entry.Path);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while listing the user: " + ex.Message);
+
+            }
+
+        }
+        static void DeleteUser(Option option, string ldapDomain, string ldapPath, string userName)
+        {
+            try
+            {
+                option.LogInformation("Creating the LDAP Connection for domain " + ldapDomain + " and LDAP://" + ldapPath);
+
+                DirectoryEntry myLdapConnection = createDirectoryEntry(ldapDomain, ldapPath);
+
+                if (myLdapConnection != null)
+                {
+                    DirectoryEntry user = myLdapConnection.Children.Find("CN=" + userName, "user");
+                    if (user != null)
+                    {
+                        option.LogInformation("Removing user: " + userName);
+                        myLdapConnection.Children.Remove(user);
+                        user.CommitChanges();
+                        option.LogInformation("User " + userName + " removed ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while removing the user: " + ex.Message);
+
+            }
+        }
+        static void CreateUser(Option option, string ldapDomain, string ldapPath, string userName, string firstName, string lastName, string description, string Password)
+        {
+            try
+            {
+                option.LogInformation("Creating the LDAP Connection for domain " + ldapDomain + " and LDAP://" + ldapPath);
+                option.LogInformation("username: " + userName + " firstName: " + firstName + " lastName: " + lastName + " description: " + description + " password: " + Password );
+
+                DirectoryEntry myLdapConnection = createDirectoryEntry(ldapDomain, ldapPath);
+                object[] password = { Password };
+                if (myLdapConnection != null)
+                {
+                    option.LogInformation("Creating user: " + userName);
+                    createUser(myLdapConnection, ldapDomain, firstName, lastName, description, password, userName, true);
+                    option.LogInformation("User " + userName + " Created" );
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while creating the user: " + ex.Message);
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Option option = Option.ParseCommandLine(args);
+            string Error = option.GetErrorMessage();
+            if(!string.IsNullOrEmpty(Error))
+            {
+                option.LogInformation("Syntax Error: " + Error);
+                option.PrintHelp();
+                return;
+            }
+            if ((option != null)&&(string.IsNullOrEmpty(Error)))
+            {
+                switch(option.ADUserAction)
+                {
+                    case Option.Action.Add:
+                        CreateUser(option, option.ldapDomain, option.ldapPath, option.userName, option.firstName, option.lastName, option.description, option.password);
+                        break;
+                    case Option.Action.List:
+                        ListUsers(option, option.ldapDomain, option.ldapPath);
+                        break;
+                    case Option.Action.Remove:
+                        DeleteUser(option, option.ldapDomain, option.ldapPath,option.userName);
+                        break;
+                    case Option.Action.Help:
+                        option.PrintHelp();
+                        break;
+                    default:
+                        option.LogInformation("ADUser: Syntax Error");
+                        option.PrintHelp();
+                        break;
+
+                }
+
+            }
+            else
+            {
+                Option o = new Option();
+                o.LogInformation("ADUser: Internal Error");
+                return;
+            }
+            return;
+
+        }
+    }
+}
